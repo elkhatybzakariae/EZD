2e2ad595b47dfc64ff2558683eb6bf1a599a4024
elkhatybzakariae <elkhatyb.zakariae@gmail.com>
2025-03-25 01:30:07 +0000

    .

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -17,25 +17,16 @@ class HomeController extends Controller
         //     ['text' => 'Villes', 'url' => null], // You can set the URL to null for the last breadcrumb
         // ];
         $projects = Project::all();
-        // dd($projects);
         return view('pages.allpr', compact('projects'));
     }
     public function project($id)
     {
-        // $breads = [
-        //     ['title' => 'Liste des Villes', 'url' => null],
-        //     ['text' => 'Villes', 'url' => null], // You can set the URL to null for the last breadcrumb
-        // ];
         $project = Project::findorfail($id);
         // dd($projects);
         return view('pages.project', compact('project'));
     }
     public function about()
     {
-        // $breads = [
-        //     ['title' => 'Liste des Villes', 'url' => null],
-        //     ['text' => 'Villes', 'url' => null], // You can set the URL to null for the last breadcrumb
-        // ];
         return view('pages.about');
     }
 }
